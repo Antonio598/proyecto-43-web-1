6d8c7c44207957b4f9b88d780ac64cc37d37dc09
Antonio <israelmayalara@gmail.com>
2026-04-18 15:45:56 -0600

    fix: remove duplicate ExperiencesGrid import in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSection from '@/components/sections/HeroSection'
 import TrustBar from '@/components/sections/TrustBar'
 import ExperiencesGrid from '@/components/sections/ExperiencesGrid'
-import ExperiencesGrid from '@/components/sections/ExperiencesGrid'
+
 import Footer from '@/components/sections/Footer'
 
 export default function Home() {
